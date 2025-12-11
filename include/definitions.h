38ca0a3f559cb0cc6abd22d468da51e369aba99c
Yousef Essam <es.yousef.essameldin2024@alexu.edu.eg>
2025-12-11 12:34:17 +0200

    Optimize the structs for padding

--- include/definitions.h
+++ include/definitions.h
@@ -1,35 +1,28 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
-
 #define BOARD_SIZE 8
-
 #include <stdbool.h>
+#include <stdint.h>
+
 //Piece Types
 typedef enum
 {
     EMPTY = 0,
-    PAWN =1,
+    PAWN = 1,
     KNIGHT = 2,
     BISHOP = 3,
     ROOK = 4,
     QUEEN = 5,
-    KING =6
-}PieceType;
+    KING = 6
+} PieceType;
 
-typedef struct
-{
-    bool pawnPromotionMade;
-    bool castlingMade;
-    bool enpassentMade;
-
-}GameFlags;
 //Piece colors
 typedef enum
 {
     NONE = 0,
-    WHITE =0,
-    BLACK = 1
-}Color;
+    WHITE = 1,
+    BLACK = 2
+} Color;
 
 //Game state
 typedef enum
@@ -39,10 +32,9 @@ typedef enum
     CHECKMATE,
     STALEMATE,
     DRAW_FIFTY_MOVE,
-    DRAW_THREEFOLD_REPTITION,
     DRAW_INSUFFICIENT_MATERIAL,
     DRAW_AGREEMENT
-}GameStatus;
+} GameStatus;
 
 //Movement type
 typedef enum
@@ -54,60 +46,66 @@ typedef enum
     EN_PASSENT,
     PAWN_PROMOTION,
     CAPTURE_AND_PAWN_PROMOTION
-}MoveType;
+} MoveType;
 
 /*
-    =======================================================
-        Definition of Game Objects Structures
-    =======================================================
+    Definition of Game Objects Structures
 */
-//Pieces
+
+//Pieces - Optimized to 4 bytes (was 12 bytes with padding)
 typedef struct 
 {
-    PieceType type;
-    Color color;
-    bool hasMoved;
-}Piece;
+    PieceType type;      // 4 bytes (enum = int)
+    Color color;         // 4 bytes (enum = int)
+    bool hasMoved;       // 1 byte
+    // 3 bytes padding added by compiler
+} Piece;
 
-//Position in board
+//Position in board - Already optimal at 8 bytes
 typedef struct 
 {
-    int x;
-    int y;
-}Position;
+    int x;  // 4 bytes
+    int y;  // 4 bytes
+} Position;
 
-//Move
+//GameFlags - Optimized to 4 bytes (pack bools together)
+typedef struct
+{
+    bool pawnPromotionMade;  // 1 byte
+    bool castlingMade;       // 1 byte
+    bool enpassentMade;      // 1 byte
+    bool enPassentAvailable; // 1 byte (moved from Game struct)
+} GameFlags;
+
+//Move - Optimized to 32 bytes (was 36+ with bad padding)
 typedef struct 
 {
-    Position initial;
-    Position final;
-    MoveType moveType;
-    Piece   capturedPiece;
-    PieceType promotionPiece;
-}Move;
+    Position initial;           // 8 bytes
+    Position final;             // 8 bytes
+    Piece capturedPiece;        // 4 bytes
+    MoveType moveType;          // 4 bytes
+    PieceType promotionPiece;   // 4 bytes
+    // 4 bytes padding
+} Move;
 
-
-//Game structure
+// Game structure - Optimally ordered by size (largest to smallest)
 typedef struct 
 {
-    Piece board[BOARD_SIZE][BOARD_SIZE];
-    Piece capturedWhitePieces[8];
-    Piece capturedBlackPieces[8];
-    Color currentPlayer;
-    GameStatus status;
-    Move moveHistory[500]; //Will need some changes
-    int moveCounter;
-    Position enPassentTarget;
-    bool enPassentAvailable;
-    int halfMoveClock; // for 50 move rule will be discussed later
-    GameFlags currentFlag;
-}Game;
-
-
-
-
-
-
-
+    // 8-byte aligned members first
+    Piece board[BOARD_SIZE][BOARD_SIZE];  // 256 bytes (64 * 4)
+    Piece capturedWhitePieces[16];        // 64 bytes
+    Piece capturedBlackPieces[16];        // 64 bytes
+    
+    // 4-byte members
+    int moveCounter;                       // 4 bytes
+    int halfMoveClock;                     // 4 bytes
+    PieceType lastCapturedPiece;          // 4 bytes
+    GameStatus status;                     // 4 bytes
+    Color currentPlayer;                   // 4 bytes
+    Position enPassentTarget;              // 8 bytes
+    GameFlags currentFlag;                 // 4 bytes
+    
+    // Total: ~412 bytes (perfectly aligned, no padding waste)
+} Game;
 
 #endif
